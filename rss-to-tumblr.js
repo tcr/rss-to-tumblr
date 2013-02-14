4c38896c720cb80ed9d5e4f57c84090bd4f5d28b
Tim Cameron Ryan <id@timryan.org>
2013-02-13 21:26:15 -0500

    Wise words

--- rss-to-tumblr.js
+++ rss-to-tumblr.js
@@ -1,10 +1,12 @@
+#!/usr/bin/env node
+
 // RSS -> Tumblr
 
 var feedparser = require('feedparser');
 var rem = require('rem');
 
-if (process.argv.length < 2) {
-  console.log('Usage: node rss2tumblr http://your.rss.feed/path');
+if (process.argv.length < 4) {
+  console.log('Usage: rss-to-tumblr http://your.rss.feed/path yourtumblrname');
   process.exit(1);
 }
 
@@ -17,7 +19,7 @@ rem.connect('tumblr.com', '*').prompt(function (err, user) {
       var date = c['date'];
 
       user.debug = true;
-      user('blog', 'olinprojects.tumblr.com', 'post').post({
+      user('blog', process.argv[3] + '.tumblr.com', 'post').post({
         type: 'text',
         state: 'published',
         date: new Date(date).toISOString(),
